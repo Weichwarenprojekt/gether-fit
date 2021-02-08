2a8d4d486ea0c47ebd4909ff582f9b17964e2606
Samuel Hübner <samu.huebner@gmail.com>
2021-02-08 20:56:28 +0100

    feat(ios-app): Add initial Xcode project

---
+++ ios-app/GetherFit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GetherFit
+//
+//  Created by Samuel Huebner on 08.02.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios-app/GetherFit/GetherFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetherFitApp.swift
+//  GetherFit
+//
+//  Created by Samuel Huebner on 08.02.21.
+//
+
+import SwiftUI
+
+@main
+struct GetherFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
